9bc3045b69f5a4f3906429fb5ab58a47e7feeb0e
Elena Yan <elena.yan@studio.unibo.it>
2022-03-31 21:50:46 +0200

    update test

--- src/test/scala/u05lab/ex1/ListTest.scala
+++ src/test/scala/u05lab/ex1/ListTest.scala
@@ -2,6 +2,7 @@ package u05lab.ex1
 
 import org.junit.Assert.{assertEquals, assertThrows}
 import org.junit.Test
+import u05lab.ex1.List.*
 
 class ListTest:
 
@@ -9,14 +10,17 @@ class ListTest:
 
   @Test
   def testZipRight(): Unit =
-    assertEquals(List((1, 0), (2, 1), (3, 2), (4, 3)), reference.zipRightRecursive)
-    assertEquals(List((1, 0), (2, 1), (3, 2), (4, 3)), reference.zipRight)
+    val expected = List((1, 0), (2, 1), (3, 2), (4, 3))
+    assertEquals(expected, reference.zipRightRecursive)
+    assertEquals(expected, reference.zipRight)
+    assertEquals(expected, reference.zipRight2)
 
   @Test
   def testPartition(): Unit =
-    assertEquals((List(2, 4), List(1, 3)), reference.partitionRecursive(_ % 2 == 0))
-    assertEquals((List(2, 4), List(1, 3)), reference.partition(_ % 2 == 0))
-    
+    val expected = (List(2, 4), List(1, 3))
+    assertEquals(expected, reference.partitionRecursive(_ % 2 == 0))
+    assertEquals(expected, reference.partition(_ % 2 == 0))
+
   @Test
   def testSpan(): Unit =
     assertEquals((List(1), List(2, 3, 4)), reference.spanRecursive(_ % 2 != 0))
@@ -28,9 +32,20 @@ class ListTest:
   def testReduce(): Unit =
     assertEquals(10, reference.reduce(_ + _))
     assertEquals(10, List(10).reduce(_ + _))
-    assertThrows(classOf[UnsupportedOperationException], () => Nil.reduce[Int](_ + _))
-    
+    assertThrows(classOf[UnsupportedOperationException], () => Nil().reduce((a, _) => a))
+
+  @Test
+  def testReduceRecursive(): Unit =
+    assertEquals(10, reference.reduceRecursive(_ + _))
+    assertEquals(10, List(10).reduceRecursive(_ + _))
+    assertThrows(classOf[UnsupportedOperationException], () => Nil().reduce((a, _) => a))
+
   @Test
   def testTakeRight(): Unit =
-    assertEquals(List(2, 3, 4), reference.takeRightRecursive(3))
-    assertEquals(List(2, 3, 4), reference.takeRight(3))
+    val expected = List(2, 3, 4)
+    assertEquals(expected, reference.takeRightRecursive(3))
+    assertEquals(expected, reference.takeRight(3))
+
+  @Test
+  def testCollect(): Unit =
+    assertEquals(List(4, 8), reference.collect({case y if y % 2 == 0 => y * 2}))
